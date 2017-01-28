a7e541a0a04d3db0b1984a952ab0e725a77e5cc9
Paweł Adamczak <pawel.ad@gmail.com>
2017-01-28 14:00:47 +0000

    Added 'redirects' AppConfig tests

---
+++ src/redirects/test/test_apps.py
@@ -0,0 +1,11 @@
+from django.apps import AppConfig
+from django.apps import apps as fakester_apps
+
+
+def test_redirects_app_config():
+    """Test 'redirects' module `AppConfig` instance"""
+    redirects_app_config = fakester_apps.get_app_config('redirects')
+
+    assert isinstance(redirects_app_config, AppConfig)
+    assert redirects_app_config.name == 'redirects'
+    assert redirects_app_config.verbose_name == 'redirects'
