e277137520be89a0d6b369835c09f98703c2619c
Paweł Adamczak <pawel.ad@gmail.com>
2017-02-06 22:36:31 +0000

    Version bump

--- fakester/fakester/__init__.py
+++ fakester/fakester/__init__.py
@@ -3,7 +3,7 @@ fakester - Rickyroll your boss while preserving the element of surprise
 """
 
 __title__ = 'fakester'
-__version__ = '0.2.1'
+__version__ = '0.3.0'
 __author__ = 'Paweł Adamczak'
 __license__ = 'MIT'
 __url__ = 'https://github.com/pawelad/fakester'
