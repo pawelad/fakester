5688310203e4b82fca1f1792b12b8bebb69d308d
Paweł Adamczak <pawel.ad@gmail.com>
2018-05-06 19:03:02 -0400

    Release version 1.1.0

--- fakester/fakester/__init__.py
+++ fakester/fakester/__init__.py
@@ -3,7 +3,7 @@ fakester - Rickyroll your boss while preserving the element of surprise
 """
 
 __title__ = 'fakester'
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 __author__ = 'Paweł Adamczak'
 __license__ = 'MIT'
 __url__ = 'https://github.com/pawelad/fakester'
