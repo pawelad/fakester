c6d846640d1d5c343b810fbc6981ea9c1874bfa7
Paweł Adamczak <pawel.ad@gmail.com>
2023-01-17 19:07:09 +0100

    Add `utils.BaseModel`

---
+++ fakester/utils/models.py
@@ -0,0 +1,23 @@
+"""
+Django models related utils.
+"""
+from django.db import models
+
+
+class BaseModel(models.Model):
+    """
+    Base Django model with shared fields.
+    """
+
+    created_at = models.DateTimeField(
+        verbose_name="created at",
+        auto_now_add=True,
+    )
+
+    modified_at = models.DateTimeField(
+        verbose_name="modified at",
+        auto_now=True,
+    )
+
+    class Meta:
+        abstract = True
