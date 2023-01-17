14cd65336f2642a2ba7f396db2dded2c8f877ee6
Paweł Adamczak <pawel.ad@gmail.com>
2023-01-17 19:10:43 +0100

    Upgrade `Django` to v6.3.0

--- fakester/fakester/wsgi.py
+++ fakester/fakester/wsgi.py
@@ -10,4 +10,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fakester.settings")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
