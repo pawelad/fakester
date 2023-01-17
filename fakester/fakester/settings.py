a92cee7a3aaf0d8ad7ac8963706fcdb07a38f485
Paweł Adamczak <pawel.ad@gmail.com>
2023-01-17 19:42:00 +0100

    Update `settings.py`

--- fakester/fakester/settings.py
+++ fakester/fakester/settings.py
@@ -1,8 +1,5 @@
 """
-Fakester application Django settings file
-
-It uses `python-decouple` and `dj-database-url` libraries to get instance
-specific config from '.env' file and environment variables
+fakester Django settings.
 """
 from pathlib import Path
 
@@ -10,114 +7,114 @@ from decouple import config
 from dj_database_url import parse as db_url
 
 
-BASE_DIR = Path(__file__).parent.parent.parent
+SRC_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = SRC_DIR.parent
 
-
-SECRET_KEY = config('SECRET_KEY')
-
-DEBUG = config('DEBUG', default=False, cast=bool)
-
-ALLOWED_HOSTS = config(
-    'ALLOWED_HOSTS',
-    default='localhost, 127.0.0.1',
-    cast=lambda l: [s.strip() for s in l.split(',')],
+SECRET_KEY = config(
+    "SECRET_KEY",
+    default="django-insecure-y+rcy_fkylgf*=h5iu%a3hijnfwj)kx=a)-!$(+!gz_t4cr!7j",
 )
 
+DEBUG = config("DEBUG", default=False, cast=bool)
+
+ALLOWED_HOSTS = config(
+    "ALLOWED_HOSTS",
+    default="localhost, 127.0.0.1, 0.0.0.0",
+    cast=lambda l: [s.strip() for s in l.split(",")],
+)
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "whitenoise.runserver_nostatic",
+    "django.contrib.staticfiles",
+    # Custom
+    "fakester",
+    "redirects",
+    # Third party
+    "bootstrap3",
+    "captcha",
+    "ratelimit",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
 
 ROOT_URLCONF = 'fakester.urls'
 
-WSGI_APPLICATION = 'fakester.wsgi.application'
-
-
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'whitenoise.runserver_nostatic',
-    'django.contrib.staticfiles',
-
-    'fakester',
-    'redirects',
-
-    'bootstrap3',
-    'captcha',
-    'ratelimit',
-]
-
-
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
+WSGI_APPLICATION = 'fakester.wsgi.application'
 
 DATABASES = {
-    'default': config(
-        'DATABASE_URL',
-        default='sqlite:///' + str(BASE_DIR.joinpath('db.sqlite3')),
+    "default": config(
+        "DATABASE_URL",
+        default="postgres://localhost/fakester",
         cast=db_url,
-    )
+    ),
 }
 
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
 
-STATIC_URL = '/static/'
-STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+LANGUAGE_CODE = "en"
 
+TIME_ZONE = "UTC"
 
-LANGUAGE_CODE = 'en'
-TIME_ZONE = 'Europe/Warsaw'
 USE_I18N = True
-USE_L10N = True
 USE_TZ = True
 
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
-# Misc
-LOGIN_URL = 'admin:login'
-LOGIN_REDIRECT_URL = 'index'
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+LOGIN_URL = "admin:login"
+LOGIN_REDIRECT_URL = "index"
 
-
-#############################
-# Third-party apps settings #
-#############################
-
-# django-recaptcha
-RECAPTCHA_PUBLIC_KEY = config('RECAPTCHA_PUBLIC_KEY', default='')
-RECAPTCHA_PRIVATE_KEY = config('RECAPTCHA_PRIVATE_KEY', default='')
-
-################
-# Own settings #
-################
+##########
+# Custom #
+##########
 AVAILABLE_DOMAINS = (
     'badsoftware.review',
     'doubledouce.club',
@@ -132,3 +129,12 @@ AVAILABLE_DOMAINS = (
     'sugarlumps.trade',
     'totallyreal.accountant',
 )
+
+
+###############
+# Third-party #
+###############
+
+# django-recaptcha
+RECAPTCHA_PUBLIC_KEY = config('RECAPTCHA_PUBLIC_KEY', default='')
+RECAPTCHA_PRIVATE_KEY = config('RECAPTCHA_PRIVATE_KEY', default='')
