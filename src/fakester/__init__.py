c6814d7ecb454d98fac8750615fb9213bd9494f1
Paweł Adamczak <pawel.ad@gmail.com>
2023-02-27 16:01:35 +0100

    Bump version

--- src/fakester/__init__.py
+++ src/fakester/__init__.py
@@ -4,7 +4,7 @@ fakester - Rickyroll your boss while preserving the element of surprise.
 
 __title__ = "fakester"
 __description__ = "Rickroll your boss while preserving the element of surprise"
-__version__ = "1.1.0"
+__version__ = "2.0.0.dev0"
 __url__ = "https://github.com/pawelad/fakester"
 __author__ = "Paweł Adamczak"
 __email__ = "pawel.ad@gmail.com"
