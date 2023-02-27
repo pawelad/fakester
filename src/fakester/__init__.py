a2a1eb14a4a1e5e1523a381c2a06f1287c552740
Paweł Adamczak <pawel.ad@gmail.com>
2023-02-27 16:00:32 +0100

    Update project attributes

--- src/fakester/__init__.py
+++ src/fakester/__init__.py
@@ -3,7 +3,9 @@ fakester - Rickyroll your boss while preserving the element of surprise.
 """
 
 __title__ = "fakester"
+__description__ = "Rickroll your boss while preserving the element of surprise"
 __version__ = "1.1.0"
-__author__ = "Paweł Adamczak"
-__license__ = "MIT"
 __url__ = "https://github.com/pawelad/fakester"
+__author__ = "Paweł Adamczak"
+__email__ = "pawel.ad@gmail.com"
+__license__ = "MPL-2.0"
